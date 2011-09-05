7659eac74f8eb95af61bc4ae34d6586bcea7dc42
chemodansama <chemodansama@gmail.com>
2011-09-05 12:05:04 +0000

    * Tmx objects xml parsing.

--- src/org/chemodansama/engine/tmx/TmxTile.java
+++ src/org/chemodansama/engine/tmx/TmxTile.java
@@ -5,18 +5,18 @@ import java.util.TreeMap;
 public class TmxTile {
     public final int id;
     
-    private final TreeMap<String, String> mParams;
+    private final TreeMap<String, String> mProperties;
     
     public TmxTile(int id) {
         this.id = id;
-        mParams = new TreeMap<String, String>();
+        mProperties = new TreeMap<String, String>();
     }
 
-    void addParam(String key, String value) {
-        mParams.put(key, value);
+    void addProperty(String key, String value) {
+        mProperties.put(key, value);
     }
     
-    public String getParam(String key) {
-        return mParams.get(key);
+    public String getProperty(String key) {
+        return mProperties.get(key);
     }
 }
